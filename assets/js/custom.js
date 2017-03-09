a3e25ec2ad0a66f8b63fc4785721e583c7a903b1
ram@codepixelz <ram@codepixelzmedia.com.np>
2017-03-09 21:16:39 +0545

    portfolio section added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,3 +44,52 @@ jQuery(function($){
 
 /* Create HTML5 element for IE */
 document.createElement("section");
+
+// WINDOW LOAD FUNCTION STARTS
+
+
+jQuery(document).ready( function() {
+    
+    var selector = jQuery(this).attr('data-filter');
+
+    var isocontainer = jQuery('.showcase');
+
+    isocontainer.imagesLoaded( function() {
+      isocontainer.masonry({
+        itemSelector: '.portfolio-item',
+      })
+    });
+
+    // var $grid = isocontainer.isotope({
+    //     itemSelector : '.portfolio-item',
+    //     masonry: {
+    //         columnWidth: 1
+    //     }
+    // });
+
+    // isocontainer.imagesLoaded().progress( function() {
+    //     isocontainer.isotope('layout');
+    // });
+
+    //Add the class selected to the item that is clicked, and remove from the others
+    var optionSets = jQuery('.portfolio-filter'),
+    optionLinks = optionSets.find('a');
+
+    optionLinks.click(function(e){
+        e.preventDefault();
+        var _this = jQuery(this);
+        
+        // don't proceed if already selected
+        if ( _this.hasClass('filter-active') ) {
+            return false;
+        }
+    
+        // var jQueryoptionSet = _this.parents('#portfolio-filter);
+        optionSets.find('.filter-active').removeClass('filter-active');
+        _this.addClass('filter-active');
+
+        //When an item is clicked, sort the items.
+        var selector = jQuery(this).attr('data-filter');
+        isocontainer.isotope({ filter: selector });
+    });
+});
